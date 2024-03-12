a9a60ceb008c2884f90b596557ebe67302a0702a
ambroise recton <ambroise@recton.com>
2024-03-12 16:48:18 +0100

    initial commit repo exo python 3

---
+++ datatypes.py
@@ -0,0 +1,9 @@
+# Tuples
+#my_tup = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+#for i in my_tup:
+#    print(i)
+
+#Dict
+my_dict = {'name':'Bronx', 'age':'2', 'occupation': "Corey's dog"}
+for key,val in my_dict.items():
+    print ("My {} is {}".format(key,val))
